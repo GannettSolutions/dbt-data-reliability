7c5ae7c412c1af49435d08ab27ff02f507740550
msg087 <123316262+msg087@users.noreply.github.com>
2026-01-14 11:44:55 -0500

    update macro table_name_with_suffix

--- macros/utils/table_operations/table_name_with_suffix.sql
+++ macros/utils/table_operations/table_name_with_suffix.sql
@@ -1,10 +1,5 @@
 {% macro table_name_with_suffix(table_name, suffix) %}
-    {% set relation_max_name_length = elementary.get_relation_max_name_length()  %}
-    {% if relation_max_name_length %}
-        {% set suffix_length = suffix | length %}
-        {% set table_name_with_suffix = table_name[:relation_max_name_length - suffix_length] ~ suffix %}
-    {% else %}
-        {% set table_name_with_suffix = table_name ~ suffix %}
-    {% endif %}
-    {{ return(table_name_with_suffix) }}
+    {% set full_name = table_name ~ suffix %}
+    {% set max_length = elementary.get_relation_max_name_length() %}
+    {{ return(elementary.truncate_table_name_for_max_length(full_name, max_length)) }}
 {% endmacro %}
