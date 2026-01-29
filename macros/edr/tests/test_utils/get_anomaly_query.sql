74a808066d9ccb7fbc26e8bf1197ae3bdbeba045
msg087 <123316262+msg087@users.noreply.github.com>
2026-01-29 09:57:12 -0500

    update get anomaly query for sqlserver

--- macros/edr/tests/test_utils/get_anomaly_query.sql
+++ macros/edr/tests/test_utils/get_anomaly_query.sql
@@ -187,26 +187,190 @@ case when
 
 
 {%- macro sqlserver__get_anomaly_query(flattened_test=none) -%}
+  {% if not flattened_test %}
+    {% set flattened_test = elementary.flatten_test(model) %}
+  {% endif %}
+  {%- set test_unique_id = flattened_test.unique_id %}
+  {%- set test_configuration = elementary.get_cache(test_unique_id) %}
+  {%- if not test_configuration %}
+      {{ exceptions.raise_compiler_error("Failed to load configuration dict from cache for test `{}`".format(test_unique_id)) }}
+  {%- endif %}
+  {%- set backfill_period = "'-" ~ test_configuration.backfill_days ~ "'" %}
+
   {%- set query -%}
-    select * from ({{ elementary.get_read_anomaly_scores_query(flattened_test) }}) results
-    where is_anomalous = 1
+    with anomaly_scores as (
+      select
+          id,
+          metric_id,
+          test_execution_id,
+          test_unique_id,
+          detected_at,
+          full_table_name,
+          column_name,
+          metric_name,
+          anomaly_score,
+          anomaly_score_threshold,
+          anomalous_value,
+          bucket_start,
+          bucket_end,
+          bucket_seasonality,
+          metric_value,
+          min_metric_value,
+          max_metric_value,
+          training_avg,
+          training_stddev,
+          training_set_size,
+          training_start,
+          training_end,
+          dimension,
+          dimension_value,
+          {{ elementary.anomaly_detection_description() }},
+          max(bucket_end) over (partition by test_execution_id) as max_bucket_end
+      from {{ elementary.get_elementary_test_table(elementary.get_elementary_test_table_name(), 'anomaly_scores') }}
+    ),
+    anomaly_scores_with_is_anomalous as (
+      select
+        *,
+        case when
+        (
+          {{ elementary.anomaly_score_condition(test_configuration) }}
+        )
+        and bucket_end > {{ elementary.edr_timeadd('day', backfill_period, 'max_bucket_end') }}
+        then 1 else 0 end as is_anomalous
+      from anomaly_scores
+    ),
+
+    final_results as (
+      select
+          metric_value as {{ elementary.escape_reserved_keywords('value') }},
+          training_avg as average,
+          {# when there is an anomaly we would want to use the last value of the metric (lag), otherwise visually the expectations would look out of bounds #}
+          case
+              when is_anomalous = 1 and '{{ test_configuration.anomaly_direction }}' = 'spike' then
+                  {{ elementary.lag('metric_value') }} over (partition by full_table_name, column_name, metric_name, dimension, dimension_value, bucket_seasonality order by bucket_end)
+              when is_anomalous = 1 and '{{ test_configuration.anomaly_direction }}' != 'spike' then
+                  {{ elementary.lag('min_metric_value') }} over (partition by full_table_name, column_name, metric_name, dimension, dimension_value, bucket_seasonality order by bucket_end)
+              when '{{ test_configuration.anomaly_direction }}' = 'spike' then metric_value
+              else min_metric_value
+          end as min_value,
+          case
+              when is_anomalous = 1 and '{{ test_configuration.anomaly_direction }}' = 'drop' then
+                  {{ elementary.lag('metric_value') }} over (partition by full_table_name, column_name, metric_name, dimension, dimension_value, bucket_seasonality order by bucket_end)
+              when is_anomalous = 1 and '{{ test_configuration.anomaly_direction }}' != 'drop' then
+                  {{ elementary.lag('max_metric_value') }} over (partition by full_table_name, column_name, metric_name, dimension, dimension_value, bucket_seasonality order by bucket_end)
+              when '{{ test_configuration.anomaly_direction }}' = 'drop' then metric_value
+              else max_metric_value
+          end as max_value,
+          bucket_start as start_time,
+          bucket_end as end_time,
+          *
+      from anomaly_scores_with_is_anomalous
+      order by bucket_end, dimension_value
+    )
+
+    select * from final_results
+    where {{ test_configuration.exclude_final_results }}
+      and is_anomalous = 1
   {%- endset -%}
   {{- return(query) -}}
 {%- endmacro -%}
 
 
 {%- macro sqlserver__get_anomaly_query_for_dimension_anomalies(flattened_test=none) -%}
-  {%- set dimension_values_query -%}
-    select distinct dimension_value from ({{ elementary.get_read_anomaly_scores_query(flattened_test) }}) results
-    where is_anomalous = 1
+  {% if not flattened_test %}
+    {% set flattened_test = elementary.flatten_test(model) %}
+  {% endif %}
+  {%- set test_unique_id = flattened_test.unique_id %}
+  {%- set test_configuration = elementary.get_cache(test_unique_id) %}
+  {%- if not test_configuration %}
+      {{ exceptions.raise_compiler_error("Failed to load configuration dict from cache for test `{}`".format(test_unique_id)) }}
+  {%- endif %}
+  {%- set backfill_period = "'-" ~ test_configuration.backfill_days ~ "'" %}
+
+  {%- set query -%}
+    with anomaly_scores as (
+      select
+          id,
+          metric_id,
+          test_execution_id,
+          test_unique_id,
+          detected_at,
+          full_table_name,
+          column_name,
+          metric_name,
+          anomaly_score,
+          anomaly_score_threshold,
+          anomalous_value,
+          bucket_start,
+          bucket_end,
+          bucket_seasonality,
+          metric_value,
+          min_metric_value,
+          max_metric_value,
+          training_avg,
+          training_stddev,
+          training_set_size,
+          training_start,
+          training_end,
+          dimension,
+          dimension_value,
+          {{ elementary.anomaly_detection_description() }},
+          max(bucket_end) over (partition by test_execution_id) as max_bucket_end
+      from {{ elementary.get_elementary_test_table(elementary.get_elementary_test_table_name(), 'anomaly_scores') }}
+    ),
+    anomaly_scores_with_is_anomalous as (
+      select
+        *,
+        case when
+        (
+          {{ elementary.anomaly_score_condition(test_configuration) }}
+        )
+        and bucket_end > {{ elementary.edr_timeadd('day', backfill_period, 'max_bucket_end') }}
+        then 1 else 0 end as is_anomalous
+      from anomaly_scores
+    ),
+
+    final_results as (
+      select
+          metric_value as {{ elementary.escape_reserved_keywords('value') }},
+          training_avg as average,
+          {# when there is an anomaly we would want to use the last value of the metric (lag), otherwise visually the expectations would look out of bounds #}
+          case
+              when is_anomalous = 1 and '{{ test_configuration.anomaly_direction }}' = 'spike' then
+                  {{ elementary.lag('metric_value') }} over (partition by full_table_name, column_name, metric_name, dimension, dimension_value, bucket_seasonality order by bucket_end)
+              when is_anomalous = 1 and '{{ test_configuration.anomaly_direction }}' != 'spike' then
+                  {{ elementary.lag('min_metric_value') }} over (partition by full_table_name, column_name, metric_name, dimension, dimension_value, bucket_seasonality order by bucket_end)
+              when '{{ test_configuration.anomaly_direction }}' = 'spike' then metric_value
+              else min_metric_value
+          end as min_value,
+          case
+              when is_anomalous = 1 and '{{ test_configuration.anomaly_direction }}' = 'drop' then
+                  {{ elementary.lag('metric_value') }} over (partition by full_table_name, column_name, metric_name, dimension, dimension_value, bucket_seasonality order by bucket_end)
+              when is_anomalous = 1 and '{{ test_configuration.anomaly_direction }}' != 'drop' then
+                  {{ elementary.lag('max_metric_value') }} over (partition by full_table_name, column_name, metric_name, dimension, dimension_value, bucket_seasonality order by bucket_end)
+              when '{{ test_configuration.anomaly_direction }}' = 'drop' then metric_value
+              else max_metric_value
+          end as max_value,
+          bucket_start as start_time,
+          bucket_end as end_time,
+          *
+      from anomaly_scores_with_is_anomalous
+      order by bucket_end, dimension_value
+    ),
+
+    dimension_values as (
+      select distinct dimension_value
+      from final_results
+      where {{ test_configuration.exclude_final_results }}
+        and is_anomalous = 1
+    )
+
+    select * from final_results
+    where {{ test_configuration.exclude_final_results }}
+      and dimension_value in (select dimension_value from dimension_values)
   {%- endset -%}
 
-  {% set dimension_anomalies_query -%}
-    select * from ({{ elementary.get_read_anomaly_scores_query(flattened_test) }}) results
-    where dimension_value in ({{ dimension_values_query }})
-  {%- endset -%}
-
-  {{- return(dimension_anomalies_query) -}}
+  {{- return(query) -}}
 {%- endmacro -%}
 
 
@@ -299,5 +463,4 @@ case when
     {%- endset -%}
     {{- return(anomaly_query) -}}
 {% endmacro %}
-
 
